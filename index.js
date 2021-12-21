64f5ed718049eeacd1420855ecbb78b64c8907c0
zynebb <zynebbouamama@hotmail.fr>
2021-12-21 16:53:24 +0100

    message

--- index.js
+++ index.js
@@ -3,18 +3,31 @@ const express = require("express");
 const formidable = require("express-formidable");
 const cors = require("cors");
 
+const api_key = process.env.MAILGUN_API_KEY; /* VOTRE CLÉ API */
+const domain = process.env.MAILGUN_DOMAIN; /* VOTRE DOMAINE SANDBOX */
+const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
+
 const app = express();
 
 app.use(cors());
 app.use(formidable());
 
 app.post("/form", (req, res) => {
-  try {
-    console.log(req.fields);
-    res.status(200).json({ message: "Server is up!" });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+  const { firstname, lastname, mail, subject, message } = req.fields;
+  /* CREATION DE L'OBJET DATA */
+  const data = {
+    from: `${firstname} ${lastname} <${mail}>`,
+    to: "zynebbouamama@hotmail.fr" /* EMAIL AVEC LAQUELLE VOUS VOUS ÊTES ENREGISTRÉS SUR MAILGUN */,
+    subject: subject,
+    text: message,
+  };
+  /* ENVOI DE L'OBJET VIA MAILGUN */
+  mailgun.messages().send(data, (error, body) => {
+    if (!error) {
+      return res.status(200).json(body);
+    }
+    res.status(401).json(error);
+  });
 });
 
 app.listen(3000, () => {
